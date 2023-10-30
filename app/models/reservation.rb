81a701e86b3b91540fa7ed1e82b02a2c324d7d0a
basem909 <basemshams30@gmail.com>
2023-10-30 12:16:54 -0300

    Add validations to reservations and user

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,45 @@
+# app/models/reservation.rb
+
 class Reservation < ApplicationRecord
+  belongs_to :user
+  belongs_to :room
+
+  validates :check_in, :check_out, presence: true
+  validate :check_in_is_before_check_out
+  validate :no_overlapping_reservations
+  validate :check_in_is_not_in_past
+
+  private
+
+  # Custom validation to ensure check_in is before check_out
+  def check_in_is_before_check_out
+    return if check_in.nil? || check_out.nil?
+
+    if check_in >= check_out
+      errors.add(:check_in, 'must be before check-out')
+    end
+  end
+
+  # Custom validation to prevent overlapping reservations for the same room
+  def no_overlapping_reservations
+    return if room.nil? || check_in.nil? || check_out.nil?
+
+    overlapping_reservations = Reservation
+      .where(room_id: room_id)
+      .where.not(id: id) # Exclude self if updating
+      .where('check_in < ? AND check_out > ?', check_out, check_in)
+
+    if overlapping_reservations.exists?
+      errors.add(:base, 'Overlapping reservation for the same room')
+    end
+  end
+
+  # Custom validation to prevent reservations in the past
+  def check_in_is_not_in_past
+    return if check_in.nil?
+
+    if check_in < Date.today
+      errors.add(:check_in, 'must not be in the past')
+    end
+  end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,6 @@
 class Room < ApplicationRecord
   has_many_attached :photos
-  has_many :reservations, dependent: :destroy
+  has_many :reservations
   has_many :users, through: :reservations
 
   validates :description, :capacity, :price, presence: true
